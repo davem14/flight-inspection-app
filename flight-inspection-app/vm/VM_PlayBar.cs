8dad6050d6db890f38441a483783cd1bc3729a07
Dov Moshe <dovbarak.moshe@live.biu.ac.il>
2021-03-29 10:44:49 +0300

    Update to playBar (view and VM)

--- flight-inspection-app/vm/VM_PlayBar.cs
+++ flight-inspection-app/vm/VM_PlayBar.cs
@@ -11,21 +11,51 @@ namespace flight_inspection_app.vm
     class VM_PlayBar : INotifyPropertyChanged
     {
         private Flight_Model model;
-        public int VM_Step { get { return this.model.Step; } }
-        public float VM_Speed { get { return this.model.Speed; } }
+        int range;
 
         public event PropertyChangedEventHandler PropertyChanged;
-        public void NotifyPropertyChanged(string propertyName)
-        {
-            if (this.PropertyChanged != null)
-                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        }
 
         public VM_PlayBar(Flight_Model model)
         {
             this.model = model;
+            this.range = model.getNumberLines();
             model.PropertyChanged +=
                 delegate (object sender, PropertyChangedEventArgs e) { this.NotifyPropertyChanged("VM_" + e.PropertyName); };
+        }
+
+        public int VM_Step { get { return this.model.Step; } }
+        public float VM_Speed { get { return this.model.Speed; } }
+
+        public int VM_Limit
+        {
+            get { return range; }
+        }
+
+        public void play()
+        {
+            model.play();
+        }
+        public void pause()
+        {
+            model.pause();
+        }
+        public void stop()
+        {
+            model.Step = 0;
+        }
+        public void Back()
+        {
+            model.Step = model.Step - 100;
+        }
+        public void Forward()
+        {
+            model.Step = model.Step + 100;
+        }
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
