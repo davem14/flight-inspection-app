a4675050a98005d16d1c2c0420d760142a333045
Dov Moshe <dovbarak.moshe@live.biu.ac.il>
2021-04-04 18:56:06 +0300

    Update: Graph

--- flight-inspection-app/view/graph.xaml.cs
+++ flight-inspection-app/view/graph.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using flight_inspection_app.model;
+using flight_inspection_app.vm;
+using OxyPlot;
+using OxyPlot.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace flight_inspection_app.view
 {
     /// <summary>
@@ -20,9 +25,54 @@ namespace flight_inspection_app.view
     /// </summary>
     public partial class graph : UserControl
     {
-        public graph()
+        private VM_Graph vm;
+        public graph(Flight_Model model, FileHandler fileHandler)
         {
             InitializeComponent();
+            vm = new VM_Graph(model, fileHandler);
+            DataContext = vm;
+
+            this.listBox1.ItemsSource = vm.Chunks;
+            //this.listBox2.ItemsSource = vm.Chunks;
+
+
+            LineAnnotation annotation = new LineAnnotation();
+            annotation.Slope = 1;
+            annotation.Intercept = 1;
+            annotation.LineStyle = LineStyle.Solid;
+            regressionLine.Annotations.Add(annotation);
+
+
+        }
+
+        string firstCategory;
+        string secondCategory;
+        bool firstIsSelected = false;
+        bool secondIsSelected = false;
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                chartFirstCategory.Title = ((Chunk)listBox1.SelectedItem).Name;
+
+                int index = listBox1.SelectedIndex;
+                if(index == 42)
+                {
+                    Console.WriteLine("hello");
+                }
+
+                vm.IndexCategory = index;
+
+                //this.vm.PointsFirstCategory = vm.getDataPoint(vm.Map[index]);
+            }
+
+            firstIsSelected = true;
+
         }
     }
+
+    
+
+
 }
--- flight-inspection-app/vm/VM_Graph.cs
+++ flight-inspection-app/vm/VM_Graph.cs
@@ -1,22 +1,219 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using flight_inspection_app.model;
+using flight_inspection_app.vm.openXML;
+using OxyPlot;
+using OxyPlot.Annotations;
 
 namespace flight_inspection_app.vm
 {
-    class VM_Graph
+    public class VM_Graph: INotifyPropertyChanged
     {
         Flight_Model model;
-        Dictionary<string, List<float>> map = new Dictionary<string, List<float>>();
+        FileHandler fileHandler;
+        public event PropertyChangedEventHandler PropertyChanged;
+        Dictionary<int, List<float>> map = new Dictionary<int, List<float>>();
+        List<string> chunksNames;
+        List<Chunk> items;
+        string xmlFileName;
 
-        VM_Graph(Flight_Model model)
+        public VM_Graph(Flight_Model model, FileHandler fileHandler)
         {
             this.model = model;
+            this.fileHandler = fileHandler;
+            OpenXML openXML = new OpenXML(this);
+            openXML.ShowDialog();
+            xmlHandler xml = new xmlHandler(xmlFileName);
+            chunksNames = xml.getList();
+
+            items = new List<Chunk>();
+            foreach (string chunk in chunksNames)
+            {
+                items.Add(new Chunk()
+                {
+                    Name = chunk
+                });
+            }
+
+            List<string> file = fileHandler.getFile();
+
+
+            int size1 = chunksNames.Count;
+            for(int i = 0; i <size1; i++)
+            {
+                List<float> list = new List<float>();
+                map.Add(i, list);
+            }
+            int size2 = file.Count;
+            for (int i = 0; i < size2; i++)
+            {
+                float[] data = Array.ConvertAll(file[i].Split(','), float.Parse);
+                int len = data.Length;
+                for(int j = 0; j < len; j++)
+                {
+                    List<float> list;
+                    map.TryGetValue(j, out list);
+                    list.Add(data[j]);
+                }
+            }
+
+            model.PropertyChanged += delegate (object sender, PropertyChangedEventArgs e)
+            {
+                if (Equals(e.PropertyName, "Step"))
+                    PointsFirstCategory = getDataPoint(map[indexCategory]);
+            };
+
+        }
+
+        int indexCategory;
+        public int IndexCategory
+        {
+            get { return this.model.Step; }
+            set
+            {
+                indexCategory = value;
+            }
+        }
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public int VM_Step
+        {
+            get { return this.model.Step; }
+        }
+
+        public List<Chunk> Chunks
+        {
+            get { return this.items; }
+        }
+
+
+        IList<DataPoint> pointsFirstCategory;
+        public IList<DataPoint> PointsFirstCategory
+        {
+            get { return pointsFirstCategory; }
+            set
+            {
+                pointsFirstCategory = value;
+                NotifyPropertyChanged("PointsFirstCategory");
+            }
+        }
+
+        IList<DataPoint> pointsSecondCategory;
+        public IList<DataPoint> PointsSecondCategory
+        {
+            get { return pointsSecondCategory; }
+            set
+            {
+                pointsSecondCategory = value;
+                NotifyPropertyChanged("pointsSecondCategory");
+            }
+        }
+
+        public List<DataPoint> getDataPoint(List<float> list)
+        {
+            List<DataPoint> listPoints = new List<DataPoint>();
+            int len = model.Step;
+            for (int i = 0; i < len; i++)
+                listPoints.Add(new DataPoint(i, list[i]));
+            return listPoints;
+        }
+
+        
+
+
+        /*string firstCategory;
+        public string FirstCategory
+        {
+            get { return firstCategory; }
+            set { firstCategory = value; }
+        }
+
+        string secondCategory;
+        public string SecondCategory
+        {
+            get { return firstCategory; }
+            set { firstCategory = value; }
+        }*/
+
+        public Dictionary<int, List<float>> Map
+        {
+            get { return this.map; }
+        }
+
+        public void setXml(string xml)
+        {
+            this.xmlFileName = xml;
         }
 
     }
 
+
+
+    class xmlHandler
+    {
+        List<string> chunksNames;
+        public xmlHandler(string fileName)
+        {
+            chunksNames = getChunksName(fileName);
+        }
+
+        List<string> getChunksName(string fileName)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@fileName);
+            int size = lines.Length;
+            int i;
+            for (i = 0; i < size; i++)
+            {
+                if (lines[i].Contains("input"))
+                    break;
+            }
+
+            List<string> names = new List<string>();
+
+            for (; i < size; i++)
+            {
+                if (lines[i].Contains("name"))
+                    names.Add(lines[i]);
+            }
+
+            int len = names.Count;
+
+            for (int j = 0; j < len; j++)
+            {
+                int index = names[j].IndexOf("<name>");
+                string s = names[j].Remove(0, index + 6);
+                names[j] = s;
+                index = names[j].IndexOf("</name>");
+                s = names[j].Remove(index);
+                names[j] = s;
+            }
+
+            return names;
+        }
+
+        public List<string> getList()
+        {
+            return chunksNames;
+        }
+
+    }
+
+    public class Chunk
+    {
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
 }
