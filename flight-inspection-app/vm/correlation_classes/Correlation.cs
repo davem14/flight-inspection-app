6b9489673fc977a0afcf36a4d2797ab8199ac098
David Emanuel <david14em@gmail.com>
2021-04-13 16:42:16 +0300

    fix correlation

--- flight-inspection-app/vm/correlation_classes/Correlation.cs
+++ flight-inspection-app/vm/correlation_classes/Correlation.cs
@@ -102,7 +102,7 @@ namespace flight_inspection_app.vm.correlation_classes
             set { regressionLine = value; }
         }
 
-        IList<Func<double, double>> normalArea;
+        IList<Func<double, double>> normalArea = new List<Func<double, double>>();
         public IList<Func<double, double>> NormalArea
         {
             get { return normalArea; }
